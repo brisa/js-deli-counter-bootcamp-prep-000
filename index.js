a808f674a30b3fdd8daefc8399aa49f4ca6f10e3
Brisa Carter <brisa.carter@gmail.com>
2019-05-19 20:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ function nowServing(katzDeliLine){
   }
 }
 function currentLine(katzDeliLine){
-  if (katzDeliLine.length > 0) {
-    for(let i = 0; i<katzDeliLine.length; i++)
-     return `The line is currently: ${i+1}+". "+${katzDeliLine[i]}.`
-  }
-  else {
-    return "The line is currently empty.";
+  var str = `The line is currently: `;
+  var newLine = [];
+  if (katzDeliLine.length === 0) return "The line is currently empty.";
+  else{
+    for (let i = 0; i < katzDeliLine.length; i++){
+        newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
+      }
+    str += newLine.join(', ')
+    return str;
   }
 }
