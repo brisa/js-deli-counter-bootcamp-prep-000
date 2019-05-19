c228ffa53d67f5f24d5959c5a3c7a20c00da912a
Brisa Carter <brisa.carter@gmail.com>
2019-05-19 20:06:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ function nowServing(katzDeliLine){
 }
 function currentLine(katzDeliLine){
   if (katzDeliLine.length > 0) {
-     return `The line is currently: ${katzDeliLine.shift()}.`
+    for(let i = 0; i<katzDeliLine.length; i++)
+     return `The line is currently: ${i+1}+". "+${katzDeliLine[i]}.`
   }
   else {
-    return "There is nobody waiting to be served!";
+    return "The line is currently empty.";
   }
 }
